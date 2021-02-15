b7c2d78c51e1a1727136f43c2cdb21440dff6464
Slava Korenblit <slavak@codevalue.com>
2021-02-15 11:49:19 +0200

    basic game run

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GameOfLife
 {
@@ -6,7 +7,49 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const uint rows = 50;
+            const uint columns = 50;
+            var board = GenerateRandomBoard(rows, columns, 1);
+            var game = new Game(rows, columns, board);
+            game.Play(10000);
+
+            Console.WriteLine($"Generation={game.Generation}, Population={game.Population}");
+
+            Console.ReadLine();
+        }
+
+        private static bool[,] GenerateRandomBoard(uint rows, uint columns, double coverage) 
+        {
+            var result = new bool[rows, columns];
+            var totalItems = (int) (rows * columns);
+            var itemsToAssign =(int) (coverage * totalItems);
+
+            var randomIndexes = GenerateRandomInts(itemsToAssign, totalItems); 
+            foreach (var index in randomIndexes)
+            {
+                result[index / rows, index % rows] = true;
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<int> GenerateRandomInts(int count, int maxValue)
+        {
+            var random = new Random();
+            // generate count random values.
+            var candidates = new HashSet<int>(count);
+            while (candidates.Count < count)
+            {
+                // May strike a duplicate.
+                candidates.Add(random.Next(maxValue));
+            }
+
+            // load them in to a list.
+            var result = new List<int>();
+            result.AddRange(candidates);
+            result.Sort();
+
+            return result;
         }
     }
 }
